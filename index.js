715d59c6ca2dac43ea7f88803cf3ffb35f431bb7
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-27 05:28:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,20 +11,85 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-}
+ var price1 = Math.floor(Math.random()*100)+1;
+ 
+ cart.push({["itemName"]:item,["itemPrice"]:price1});
 
+ return `${item} has been added to your cart.`;
+}
+ 
 function viewCart() {
-  // write your code here
+ var string;
+ if(cart.length===0){
+    return "Your shopping cart is empty.";
+ }else{ 
+    var next = "";
+   
+   for(var i = 0;i<cart.length;i++){
+     
+     if(cart.length ===1){
+       return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`
+     }
+   else if(i===cart.length -2){
+      next =`, and ${cart[i+1].itemName} at $${cart[i+1].itemPrice}.`
+     
+      return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}${next}`;
+     
+   
+    }else{
+      next =`, ${cart[i+1].itemName} at $${cart[i+1].itemPrice} `;
+        return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}${next}`;
+    }
+  
+     
+   }
+   
+  
+ }
 }
 
 function total() {
   // write your code here
+  var total=0;
+  for(var i =0;i<cart.length;i++){
+    if(!isNaN(cart[i].itemPrice)){
+      total+=cart[i].itemPrice;
+    }
+    
+  }
+  
+  
+  return total;
 }
 
+var found=false;
 function removeFromCart(item) {
   // write your code here
+   for(var i= 0;i< cart.length; i++){
+     if(cart[i].itemName ===item){
+       cart.splice(i,1);
+       found=true;
+       return cart;
+       
+     }
+     
+     
+     if(found ===false && i===cart.length -1 ){
+       return "That item is not in your cart.";
+     }
+   }
 }
+
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(arguments.length===0 || isNaN(cardNumber)){
+    return("Sorry, we don\'t have a credit card on file for you.");
+  }else{
+   
+    var it =total();
+     cart.length = 0;
+    return `Your total cost is $${it}, which will be charged to the card ${cardNumber}.`;
+  }
+  
 }
